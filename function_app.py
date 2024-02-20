0f2583a1fb6df5dac9c4ef377c86079b603dd194
doexperienceit <narsimhareddyvijay@gmail.com>
2024-02-19 21:14:24 -0500

    testing fucntional

--- function_app.py
+++ function_app.py
@@ -20,6 +20,6 @@ def func_testinggithubactions(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully by Vijay. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully by Vijay today. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
